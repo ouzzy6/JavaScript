823595a428f9d3d86f7c2f605be55815bff4ab47
ouzzy6 <145574483+ouzzy6@users.noreply.github.com>
2024-05-06 14:14:59 +0200

    Add files via upload

---
+++ objectMethods.js
@@ -0,0 +1,13 @@
+let person = {
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+}
+
+let keys = Object.keys(person);
+console.log(keys)
+
+let values = Object.values(person);
+console.log(values);
+
+//En este ejemplo usamos .keys y .values para devolver los resultados.
